e6c007bdc27427636b455cff4c5c756f377fc73e
liuqingwen <liuqingwen@qiyi.com>
2022-08-19 15:22:31 +0800

    part

---
+++ namesrv/src/test/java/org/apache/rocketmq/ct/CodeTest.java
@@ -0,0 +1,21 @@
+package org.apache.rocketmq.ct;
+
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * @author liuqingwen@qiyi.com
+ * @date 2021/12/1 06:43
+ */
+public class CodeTest {
+
+    @Test
+    public void test() {
+
+        System.out.println(File.separator);
+
+    }
+
+
+}
